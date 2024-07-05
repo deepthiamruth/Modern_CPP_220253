1c177f9a23a6a890577c8314b60025a8ce01ab17
Amruth Deepthi <amruthd@kpit.com>
2024-07-05 11:34:50 +0530

    smart_pointers

---
+++ 5_JULY/smart_pointers.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <memory> 
+
+
+class Data
+{
+private:
+    int m_value{0};
+public:
+    Data()=default;
+    Data(const Data & ) = default; // if we enable we dont get error at Magic(d1);
+    Data( Data&& ) = delete;
+    Data &  operator = (const Data& ) = delete;
+    Data & operator = (Data && ) = delete;
+    ~Data()= default;
+
+    // Data(int val ): m_value {val}{
+    // }
+
+    explicit Data(int val) :m_value{val}{
+    } //explicit constructor cannot be used for auto conversion
+
+    friend std::ostream &operator<<(std::ostream &os, const Data &rhs) {
+        os << "m_value: " << rhs.m_value;
+        return os;
+    }
+};
+
+
+int main(){
+    int32_t x{10};
+    char grade {'A'};
+    //     int* ptr = (int*) malloc(4);
+    //     char* ptr2 = (char*)malloc(1);
+
+    std::unique_ptr <int> ptr1 {(int*) malloc(4)};
+    std::unique_ptr <int> ptr2 {new int()};
+    std::unique_ptr <char> ptr3 {(char*) malloc(1)};
+
+    std:: unique_ptr <Data> ptr4{new Data{100}};
+    //dont make l value raw pointers
+
+}
+
